6d80913c772284a5501638b1aef9cf1165ce4972
SpaceBaker <emile.forcier@gmail.com>
2025-10-07 16:16:01 -0400

    build: change default compiler
    - Changed to glibc to make it 'easier' for now
    - add umask 022
    - add more env var to toolchain bin

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -11,7 +11,7 @@
 ##################################################################
 
 # This is the only variables you should have to modify
-TARGET=arm-none-linux-musleabihf
+TARGET=arm-xilux-linux-gnueabihf
 CROSS_COMPILER_PATH="${HOME}/x-tools/${TARGET}"
 CROSS_COMPILER_SYSROOT="${CROSS_COMPILER_PATH}/${TARGET}/sysroot"
 
@@ -24,6 +24,9 @@ fi
 # Disable and clear shell hash to ensure that updated PATH is always used
 set +h
 hash -r
+
+# Make sure that newly created files/directories are writable only by the owner
+umask 022
 
 # Get the directory of this script, even if it's a symlink
 get_script_dir()
@@ -54,25 +57,33 @@ TOP_DIR=$(dirname "${SCRIPT_DIR}")
 TARGET_ROOTFS="${TOP_DIR}/root"
 SRC_DIR="${TOP_DIR}/sources"
 PATH="${CROSS_COMPILER_PATH}/bin:${SCRIPT_DIR}:${PATH}"
-
 CHOST="${TARGET}"
 ARCH="$(echo "${TARGET}" | cut -d '-' -f1)"
 CROSS_COMPILE="${TARGET}-"
-CC="${CROSS_COMPILE}gcc"
-CXX="${CROSS_COMPILE}g++"
-LD="${CROSS_COMPILE}ld"
+# Binutils
+CC="${TARGET}-gcc"
+CXX="${TARGET}-g++"
+CPP="${TARGET}-gcc -E"
+AR="${TARGET}-ar"
+AS="${TARGET}-as"
+LD="${TARGET}-ld"
+RANLIB="${TARGET}-ranlib"
+READELF="${TARGET}-readelf"
+STRIP="${TARGET}-strip"
+# Make flags
 MAKEFLAGS="-j$(nproc)"
 # Pre-processor flags
-CPPFLAGS="-I${TARGET_ROOTFS}/usr/include"
+# CPPFLAGS="-I${TARGET_ROOTFS}/usr/include"
 # C flags
 # CFLAGS="-I${TARGET_ROOTFS}/usr/include"
 # C++ flags
-# C++FLAGS="-I${TARGET_ROOTFS}/usr/include"
+# CXXFLAGS="-I${TARGET_ROOTFS}/usr/include"
 # Linker flags
-LDFLAGS="-L${TARGET_ROOTFS}/usr/lib"
-# LD_LIBRARY_PATH="${TARGET_ROOTFS}/usr/lib"
-export TOP_DIR SCRIPT_DIR TARGET_ROOTFS SRC_DIR TARGET CROSS_COMPILER_PATH CROSS_COMPILER_SYSROOT \
-			 ARCH CC CXX LD CHOST CROSS_COMPILE MAKEFLAGS LDFLAGS CPPFLAGS PATH
+# LDFLAGS="-L${TARGET_ROOTFS}/usr/lib"
+# LD_LIBRARY_PATH="${TARGET_ROOTFS}/usr/lib"\
+
+export SCRIPT_DIR TOP_DIR TARGET_ROOTFS SRC_DIR PATH CHOST ARCH CROSS_COMPILE \
+			 CC CXX CPP AR AS LD RANLIB READELF STRIP MAKEFLAGS
 
 echo -e "New environment is :\n\
 	TOP_DIR=${TOP_DIR}\n\
@@ -90,6 +101,8 @@ echo -e "New environment is :\n\
 	CROSS_COMPILE=${CROSS_COMPILE}\n\
 	MAKEFLAGS=${MAKEFLAGS}\n\
 	CPPFLAGS=${CPPFLAGS}\n\
+	CFLAGS=${CFLAGS}\n\
+	CXXFLAGS=${CXXFLAGS}\n\
 	LDFLAGS=${LDFLAGS}\n\
 	PATH=${PATH}"
 # LD_LIBRARY_PATH=${LD_LIBRARY_PATH}\n\
